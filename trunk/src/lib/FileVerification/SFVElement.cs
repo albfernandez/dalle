9b7d767cddbc51a92622b386d40521e155645e6d
infjaf <infjaf@6e796c5a-8f20-4349-956d-9a2269c5cb8f>
2004-02-12 09:35:17 +0000

    Soporte para archivos de almacenamiento de hash para verificación de archivos.

---
+++ trunk/src/lib/FileVerification/SFVElement.cs
@@ -0,0 +1,41 @@
+
+using Dalle.FileVerification.FileHashers;
+
+namespace Dalle.FileVerification
+{
+
+	public class SFVElement {
+	
+		private string file;
+		private string hash;
+		private IFileHasher hasher;
+		private string realHash;
+	
+		public SFVElement (string file, string hash, IFileHasher hasher)
+		{
+			this.file = file;
+			this.hash = hash;
+			this.hasher = hasher;
+		}
+		
+		public string FileName {
+			get { return file; }
+		}
+		public string ExpectedHash {
+			get { return hash; }
+		}
+		public string RealHash {
+			get { return realHash; }
+		}
+		public string GenerateHash ()
+		{
+			realHash = hasher.GenerateHash (file);
+			return realHash;
+		}
+		public bool IsOk ()
+		{
+			realHash = GenerateHash();
+			return (realHash == hash);
+		}
+	}
+}
---
+++ trunk/src/lib/FileVerification/Verifiers/MD5Verifier.cs
@@ -0,0 +1,102 @@
+/*
+
+	Dalle - A split/join file utility library
+	Dalle.FileVerification.Verifiers.MD5Verifier - 	
+		Verification of md5 files
+	
+    Copyright (C) 2003  Alberto Fernández <infjaf00@yahoo.es>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+
+using System.Collections;
+using System.IO;
+using System;
+
+using Dalle.FileVerification;
+using Dalle.FileVerification.FileHashers;
+
+
+namespace Dalle.FileVerification.Verifiers
+{
+
+
+	public class MD5Verifier : FVerification
+	{
+		public MD5Verifier (): base ("md5", true)
+		{
+		}
+		protected MD5Verifier (string name, bool canCreate): base (name, canCreate)
+		{
+		}
+		protected override ArrayList GenerateSFVFileList (string file)
+		{
+			ArrayList ret = new ArrayList ();
+			TextReader reader = File.OpenText (file);
+			string linea;
+			linea = reader.ReadLine ();
+			while (linea != null){				
+				string fname = linea.Substring (linea.IndexOf (" ")).Trim();
+				string hash = linea.Substring (0, linea.IndexOf(" ")).Trim();
+				fname = fname.Replace ('/', Path.DirectorySeparatorChar);
+				SFVElement el = new SFVElement (fname, hash, new FileHasherMd5()); 
+				ret.Add (el);
+				
+				linea = reader.ReadLine ();
+			}
+			return ret;
+		}
+		public override void CreateSFV (string[] files, bool recursive, TextWriter writer)
+		{
+			CreateRecursive (files, recursive, writer);			
+		}
+		protected void CreateRecursive (string[] files, bool recursive, TextWriter writer)
+		{
+			foreach (string f in files){
+				if (Directory.Exists (f)){
+					if (recursive)
+						CreateRecursive (Directory.GetFileSystemEntries (f), recursive, writer);					
+				}
+				else {
+					SFVElement e = new SFVElement (f, "", new FileHasherMd5());
+					e.GenerateHash();
+					PutMd5 (e, writer);										
+				}
+			}
+		}
+		protected virtual void PutMd5 (SFVElement e, TextWriter writer)
+		{
+			writer.WriteLine (e.RealHash + "  " + e.FileName);		
+		}
+		public override bool IsFormatRecognized (string file)
+		{
+			if (!file.ToUpper().EndsWith(".MD5"))
+				return false;
+			try{
+				StreamReader reader = File.OpenText (file);
+				string st = reader.ReadLine();
+				reader.Close();
+				Console.WriteLine ("" + st.IndexOf(" "));
+				return (st.IndexOf(" ") == 32);
+			}
+			catch (Exception){
+				
+			}
+			return false;
+		}
+	}
+}
