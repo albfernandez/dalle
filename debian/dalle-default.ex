e486f138965fe4bbfc4403664049b0181b63049f
Alberto Fernández <infjaf@gmail.com>
2004-09-24 12:49:14 +0000

    Soporte para paquetes de debian

---
+++ debian/dalle-default.ex
@@ -0,0 +1,10 @@
+# Defaults for dalle initscript
+# sourced by /etc/init.d/dalle
+# installed at /etc/default/dalle by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
