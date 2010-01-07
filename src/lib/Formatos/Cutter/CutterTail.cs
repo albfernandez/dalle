81f7c1e657faf3334a16f94210c93cecd78dcdee
Alberto Fernández <infjaf@gmail.com>
2010-01-07 20:53:29 +0000

    Gcutter support

--- src/lib/Formatos/Cutter/CutterTail.cs
+++ src/lib/Formatos/Cutter/CutterTail.cs
@@ -3,7 +3,7 @@
 	Dalle - A split/join file utility library
 	Dalle.Formatos.Cutter.CutterTail - Basic support for cutter files.
 	
-    Copyright (C) 2003-2009  Alberto Fernández <infjaf00@yahoo.es>
+    Copyright (C) 2003-2010  Alberto Fernández <infjaf00@yahoo.es>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -19,7 +19,20 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
-
+/*
+struct cutterautoexe	{
+						char version[10];
+						char crc[50];//del fragmento
+						char originalfile[4097];
+						long long numberofparts;
+						long long exesize; // size of the exe file,in bytes // // Siempre 0
+						long long filesize; // size of the packed file,in bytes //
+						char ostype[100]; // Operating system typ ; for auto-exe files//
+						char cputype[50]; // processor ; for auto-exe files//
+						char what[23]; // used to control if it's a valid cutter stuff:-) //
+						//record the char* "ohmygodiamacutterstuff" in stuffdata.what , else it will be invalid! //
+						};
+			*/
 using Dalle.Utilidades;
 using System.IO;
 using System;
@@ -28,6 +41,7 @@ namespace Dalle.Formatos.Cutter {
 
 
 	public class CutterTail {
+
 		public const int TAIL_SIZE = 4360;
 		public const string WHAT = "ohmygodiamacutterstuff";
 		private long version = 0;
@@ -78,33 +92,47 @@ namespace Dalle.Formatos.Cutter {
 		
 		public CutterTail (){
 		}
-		public static CutterTail LoadFromFile (string fichero) {
-			FileStream reader = new FileStream (fichero, FileMode.Open);
-			if (reader.Length <= TAIL_SIZE){
-				reader.Close();
+		public static CutterTail LoadFromFile (string fichero)
+		{
+			try 
+			{
+				if (!File.Exists (fichero))
+				{
+					return null;
+				}
+				FileInfo fi = new FileInfo (fichero);
+				if (fi.Length <= TAIL_SIZE) 
+				{
+					return null;
+				}
+				FileStream reader = new FileStream (fichero, FileMode.Open);
+				reader.Seek (fi.Length - TAIL_SIZE, SeekOrigin.Begin);
+				
+				byte[] buffer = new byte[TAIL_SIZE];
+				reader.Read (buffer, 0, buffer.Length);
+				reader.Close ();
+				
+				CutterTail ret = new CutterTail ();
+
+				ret.version = Convert.ToInt64 (UtArrays.LeerTexto (buffer, 0, 10));
+				ret.crc = Convert.ToInt64 (UtArrays.LeerTexto (buffer, 10, 50));
+				ret.original = UtArrays.LeerTexto (buffer, 60, 4097);
+				ret.numberOfParts = UtArrays.LeerInt64 (buffer, 4160);
+				ret.exeSize = UtArrays.LeerInt64 (buffer, 4168);
+				ret.fileSize = UtArrays.LeerInt64 (buffer, 4176);
+				ret.osType = UtArrays.LeerTexto (buffer, 4184, 100);
+				ret.cpuType = UtArrays.LeerTexto (buffer, 4284, 50);
+				string what = UtArrays.LeerTexto (buffer, 4334, 23);
+				if (what != WHAT)
+					return null;
+				return ret;
+			}
+			catch (Exception e) 
+			{
+				Console.WriteLine (e.StackTrace);
 				return null;
 			}
-			reader.Seek (reader.Length - TAIL_SIZE, SeekOrigin.Begin);
-			
-			byte[] buffer = new byte[TAIL_SIZE];
-			reader.Read (buffer, 0, TAIL_SIZE);
-			reader.Close();		
-			
-			CutterTail ret = new CutterTail();
-			
-			ret.version = Convert.ToInt64(UtArrays.LeerTexto (buffer, 0));
-			ret.crc = Convert.ToInt64(UtArrays.LeerTexto (buffer, 10));
-			ret.original = UtArrays.LeerTexto(buffer, 60);
-			ret.numberOfParts = UtArrays.LeerInt64 (buffer, 4157);
-			ret.exeSize = UtArrays.LeerInt64 (buffer, 4165);
-			ret.fileSize = UtArrays.LeerInt64(buffer, 4173);
-			ret.osType = UtArrays.LeerTexto(buffer, 4181);
-			ret.cpuType= UtArrays.LeerTexto(buffer, 4281);
-			
-			string what = UtArrays.LeerTexto(buffer, 4331);
-			if (what != WHAT)
-				return null;
-			return ret;
+
 		}
 		public byte[] ToByteArray () {
 			
