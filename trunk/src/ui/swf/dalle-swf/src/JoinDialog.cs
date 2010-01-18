a9fde3e0bcc510ebd2c426abb566b0f6b645ccf5
infjaf <infjaf@6e796c5a-8f20-4349-956d-9a2269c5cb8f>
2010-01-18 19:48:36 +0000

    Join support on dalle-swf

--- trunk/src/ui/swf/dalle-swf/src/JoinDialog.cs
+++ trunk/src/ui/swf/dalle-swf/src/JoinDialog.cs
@@ -23,7 +23,10 @@ using System;
 using System.Windows.Forms;
 using System.Drawing;
 using System.ComponentModel;
+using System.Threading;
 
+using Dalle;
+using Dalle.Formatos;
 
 
 namespace Dalle.UI.DalleSWF
@@ -32,6 +35,16 @@ namespace Dalle.UI.DalleSWF
 	public class JoinDialog : Form
 	{
 	
+		private Label label1;
+		private OpenFileDialog openFileDialog1;
+		private TextBox txtFilename;
+		private Button btnBrowse;
+		private ProgressBar pb;
+		private Button btnClose;
+		private Button btnJoin;
+		private Thread t;
+		private bool requestStop = false;
+		
 		private static JoinDialog instance = null;
 		
 		public static JoinDialog Instance {
@@ -48,16 +61,161 @@ namespace Dalle.UI.DalleSWF
 			InitComponent();
 			this.Closing += new CancelEventHandler (closing);
 		}	
-		private void InitComponent()
+		private void InitComponent ()
 		{
+			this.MinimizeBox = false;
+			this.MaximizeBox = false;
 			this.Size = new Size (400, 300);
 			//this.Text = Catalog.GetString("Merge");
 			this.Text = "Merge";
+			
+			this.SuspendLayout ();
+			
+			this.label1 = new Label ();
+			this.label1.Location = new System.Drawing.Point (16, 16);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size (160, 16);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Select the file to Join:";
+			
+			this.txtFilename = new TextBox ();
+			this.txtFilename.Location = new System.Drawing.Point (8, 32);
+			this.txtFilename.Name = "txtFilename";
+			this.txtFilename.Size = new System.Drawing.Size (208, 20);
+			this.txtFilename.TabIndex = 10;
+			this.txtFilename.Text = "";
+			this.txtFilename.TextChanged += new System.EventHandler (this.txtFilename_TextChanged);
+			
+			this.btnBrowse = new Button ();
+			this.btnBrowse.Location = new System.Drawing.Point (232, 32);
+			this.btnBrowse.Name = "cmdBrowse";
+			this.btnBrowse.Size = new System.Drawing.Size (96, 24);
+			this.btnBrowse.TabIndex = 1;
+			this.btnBrowse.Text = "Browse...";
+			this.btnBrowse.Click += new System.EventHandler (this.btnBrowseClick);
+			
+			this.pb = new ProgressBar ();
+			this.pb.Location = new System.Drawing.Point (8, 70);
+			this.pb.Name = "pb";
+			this.pb.Size = new System.Drawing.Size (320, 24);
+			this.pb.Step = 1;
+			this.pb.TabIndex = 11;
+			this.pb.Minimum = 0;
+			this.pb.Maximum = 100;
+			this.pb.Value = 0;
+			//this.pb.Visible = false;
+			
+			this.btnClose = new Button ();
+			this.btnClose.Location = new Point (250, 100);
+			this.btnClose.Size = new System.Drawing.Size (50, 24);
+			this.btnClose.Text = "Close";
+			this.btnClose.Click += new EventHandler (btnCloseClick);
+			
+			this.btnJoin = new Button ();
+			this.btnJoin.Location = new Point (300, 100);
+			this.btnJoin.Size = new System.Drawing.Size (50, 24);
+			this.btnJoin.Text = "Join";
+			this.btnJoin.Click += new EventHandler (btnJoinClick);
+			
+			this.Controls.AddRange (new Control[] {
+				label1,
+				txtFilename,
+				btnBrowse,
+				pb,
+				btnClose,
+				btnJoin
+				
+			});
+			Manager.Instance.Progress += new ProgressEventHandler (this.OnProgress);
+			this.ResumeLayout (false);
+			
 		}
+		protected virtual void OnProgress (long done, long total)
+		{
+			double fraction = ((double) done) / ((double)total);
+			Console.WriteLine("progreso=" + done + "/" + total +"=" + fraction);
+			this.pb.Value = (int) Math.Floor(100*fraction);
+			Console.WriteLine("barra=" + this.pb.Value);
+			//this.Refresh();
+			System.Threading.Thread.Sleep (500);
+			if (requestStop)
+			{
+				Manager.Instance.Stop();
+			}
+		}
+		
 		private void closing (object sender, CancelEventArgs args)
 		{
-			this.Visible = false;
-			args.Cancel = true;			
+			if (t != null) {
+				DialogResult dr = MessageBox.Show ("Join operation running, Stop?", "Stop?", MessageBoxButtons.YesNo);
+				if (dr == DialogResult.Yes) {
+					this.requestStop = true;
+				}
+			
+			} else {
+				this.Visible = false;
+			}
+			args.Cancel = true;
+		}	
+		private void btnCloseClick (object sender, EventArgs args)
+		{
+			if (t != null)
+			{
+				DialogResult dr = 
+					MessageBox.Show ("Join operation running, Stop?", "Stop?", MessageBoxButtons.YesNo);
+				if (dr == DialogResult.Yes) {
+					this.requestStop = true;
+				}
+				
+			}
+			else {
+				this.Visible = false;			
+			}
+		}
+		
+		private void btnJoinClick (object sender, EventArgs args)
+		{
+			if (Manager.Instance.GetFormatoFichero (txtFilename.Text) == null) {
+				MessageBox.Show ("File format not supported", "Done");
+				return;
+			}
+			if (t == null)
+			{
+				this.requestStop = false;
+				this.t = new Thread(new ThreadStart(joinFile));
+				this.t.Start();
+			}
+			
+		}
+		protected void joinFile ()
+		{
+			Manager.Instance.Progress += new ProgressEventHandler (this.OnProgress);
+			Manager.Instance.Unir (txtFilename.Text);
+			this.requestStop = false;
+			t = null;
+		}
+		
+		
+		/// <summary>
+		/// Click handler for the cmdBrowse button.
+		/// Browses for the input file to be chopped.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btnBrowseClick (object sender, System.EventArgs e)
+		{
+			if (openFileDialog1 == null) {
+				openFileDialog1 = new OpenFileDialog ();
+			}
+			openFileDialog1.CheckFileExists = true;
+			openFileDialog1.Filter = "All files (*.*)|*.*";
+			openFileDialog1.ShowDialog ();
+
+			txtFilename.Text = openFileDialog1.FileName;
+		}	
+		private void txtFilename_TextChanged(object sender, System.EventArgs e)
+		{
+			//p_UpdateStatus();
 		}	
 	
 	}
