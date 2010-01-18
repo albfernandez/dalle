5185df511914df49c5754b379ac79bb7fd8ac3ad
Alberto Fernández <infjaf@gmail.com>
2010-01-18 22:58:08 +0000

    Email updated

--- src/ui/gtk/dalle-gtk/src/CustomProgressBar.cs
+++ src/ui/gtk/dalle-gtk/src/CustomProgressBar.cs
@@ -4,7 +4,7 @@
 	Dalle.UI.DalleGtk.CustomProgressBar - 
 		ProgressBar that shows text percentage.
 		
-    Copyright (C) 2003-2009  Alberto Fernández <infjaf00@yahoo.es>
+    Copyright (C) 2003-2010  Alberto Fernández <infjaf@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
